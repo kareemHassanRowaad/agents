5612e3f6e1874734cabdf340063ddbea3dc2d9a5
kareem hassan <k.hasan@rowaad.net>
2025-08-04 19:17:30 +0300

    app

--- agent_update.py
+++ agent_update.py
@@ -1,7 +1,11 @@
+__import__('pysqlite3')
+import sys
+
 import streamlit as st
 from crewai import LLM,Agent, Task, Crew
 import os
 
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 misteral_key = "aSTlmVLcWwKMH3kCNXGX25U1CKwMNAfo"
 
